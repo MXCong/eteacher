6a4aa7ed35b1f9455f9eb65eb42af25372b40c7b
lifeidroid <lifeidroid@Gmail.com>
2017-03-13 17:13:01 +0800

    1、代码添加

---
+++ eteacher/src/com/turing/eteacher/dao/AnswerRecordDAO.java
@@ -0,0 +1,15 @@
+package com.turing.eteacher.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.turing.eteacher.base.BaseDAO;
+import com.turing.eteacher.model.AnswerRecord;
+/**
+ * 
+ * @author lifei
+ *
+ */
+@Repository
+public class AnswerRecordDAO extends BaseDAO<AnswerRecord> {
+
+}
---
+++ eteacher/src/com/turing/eteacher/dao/OptionsDAO.java
@@ -0,0 +1,11 @@
+package com.turing.eteacher.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.turing.eteacher.base.BaseDAO;
+import com.turing.eteacher.model.Options;
+
+@Repository
+public class OptionsDAO extends BaseDAO<Options> {
+
+}
